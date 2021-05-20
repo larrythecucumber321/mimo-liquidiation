d7825776cd3b0a0b6c620322fadf441ede3aa050
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-05-20 11:01:19 -0700

    initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,52 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { Opened, Deposited, Withdrawn, Borrowed, Repaid } from '../generated/VaultsCore/VaultsCore'
+import { Vault  } from '../generated/schema'
+import { VaultsDataProvider } from '../generated/VaultsCore/VaultsDataProvider'
+import { BigInt, Address } from "@graphprotocol/graph-ts";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+let ZERO = BigInt.fromI32(0);
+let VAULT_DATA_PROVIDER = "0x9C29d8D359255e524702c7A9c95C6e6ae38274Dc";
+
+export function handleDeposit(event: Deposited): void {
+  let vault = getOrCreateVault(event.params.vaultId.toString(), event.params.sender);
+  vault.supplied = vault.supplied.plus(event.params.amount);
+  vault.save();
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleWithdraw(event: Withdrawn): void {
+  let vault = getOrCreateVault(event.params.vaultId.toString(), event.params.sender);
+  vault.supplied = vault.supplied.minus(event.params.amount);
+  vault.save();
+}
+
+export function handleBorrow(event: Borrowed): void {
+  let vault = getOrCreateVault(event.params.vaultId.toString(), event.params.sender);
+  vault.borrowed = vault.borrowed.plus(event.params.amount);
+  vault.save();
+}
+
+export function handleRepaid(event: Repaid): void {
+  let vault = getOrCreateVault(event.params.vaultId.toString(), event.params.sender);
+  vault.borrowed = vault.borrowed.minus(event.params.amount);
+  vault.save();
+}
+
+const getOrCreateVault = (vaultId: string, owner: Address ): Vault => {
+  let vault = Vault.load(vaultId);
+  let vaultData = VaultsDataProvider.bind(Address.fromString(VAULT_DATA_PROVIDER));
+
+  if (vault == null) {
+    vault = new Vault(vaultId);
+    vault.owner = null;
+    vault.collateralType = null;
+    vault.borrowed = ZERO;
+    vault.supplied = ZERO;
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  
+  let collateralType = vaultData.try_vaultCollateralType(BigInt.fromString(vaultId));
+  vault.owner = owner;
+  vault.collateralType = !collateralType.reverted ? collateralType.value : vault.collateralType;
+  vault.borrowed = vault.borrowed;
+  vault.supplied = vault.supplied;
+
+  return vault as Vault;
 }
